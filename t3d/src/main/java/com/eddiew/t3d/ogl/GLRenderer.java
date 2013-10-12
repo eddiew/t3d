f0898957764fc3afd455df4e12a58e2f1f3d3650
eddiew <eddie.wang@frontier.com>
2013-10-12 16:19:26 -0700

    the stupid thing sort of works now

--- t3d/src/main/java/com/eddiew/t3d/ogl/GLRenderer.java
+++ t3d/src/main/java/com/eddiew/t3d/ogl/GLRenderer.java
@@ -3,8 +3,14 @@ package com.eddiew.t3d.ogl;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.util.Log;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -13,19 +19,37 @@ import javax.microedition.khronos.opengles.GL10;
  * Created by Eddie on 10/11/13.
  */
 public class GLRenderer implements GLSurfaceView.Renderer {
+
     float[] viewMatrix = new float[16];
     float[] projectionMatrix = new float[16];
-    public ArrayList<GLObject> glObjects = new ArrayList<GLObject>();
+    float[] vpMatrix = new float[16];
+    volatile public HashMap<Integer,GLObject> glObjects = new HashMap<Integer,GLObject>();
+    volatile public ArrayList<ObjectData> rawData = new ArrayList<ObjectData>();
 
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
         // Set the background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         //initialize view matrix
-        final float camX = 0f, camY = 0f, camZ = 1.5f;
-        final float lookX = 0f, lookY = 0f, lookZ = -1f;
+        final float camX = 0f, camY = 0f, camZ = -3f;
+        final float lookX = 0f, lookY = 0f, lookZ = 0f;
         final float upX = 0f, upY = 1f, upZ = 0f;
         Matrix.setLookAtM(viewMatrix, 0, camX, camY, camZ, lookX, lookY, lookZ, upX, upY, upZ);
+//        //Test stuff
+//        final float[] modelMatrix = new float[16];
+//        Matrix.setIdentityM(modelMatrix,0);
+//        addObject( new float[]{
+//                0f, 0.622008459f, 0,
+//                1, 0, 0, 1,
+//
+//                -0.5f, -0.311004243f, 0,
+//                0, 1, 0, 1,
+//
+//                0.5f, -0.311004243f, 0,
+//                0, 0, 1, 1
+//        }, new short[]{
+//                0, 1, 2
+//        }, modelMatrix);
     }
 
     @Override
@@ -35,17 +59,198 @@ public class GLRenderer implements GLSurfaceView.Renderer {
         final float ratio = (float) width / height;
         final float left = -ratio, right = ratio;
         final float bottom = -1.0f, top = 1.0f;
-        final float near = 1.0f, far = 1000.0f;
+        final float near = 3f, far = 7f;
         Matrix.frustumM(projectionMatrix, 0, left, right, bottom, top, near, far);
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
+        if(!rawData.isEmpty()){
+            processRawData();
+            rawData.clear();
+        }
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        for(GLObject o : glObjects){
-            o.draw(viewMatrix, projectionMatrix);
+        Matrix.multiplyMM(vpMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
+        for(GLObject o : glObjects.values()){
+            o.draw(vpMatrix);
         }
     }
 
+    /**
+     * Compiles GLSL
+     * @param type Can be vertex shader (GLES20.GL_VERTEX_SHADER) or fragment shader (GLES20.GL_FRAGMENT_SHADER)
+     * @param shaderCode The GLSL shader code to be compiled
+     * @return
+     */
+    public static int loadShader(int type, String shaderCode){
+        int shader = GLES20.glCreateShader(type);
+        if(shader == 0){
+            throw new RuntimeException("Error creating shader\n" + shaderCode);
+        }
+        GLES20.glShaderSource(shader,shaderCode);
+        GLES20.glCompileShader(shader);
+
+        // Get the compilation status.
+        final int[] compileStatus = new int[1];
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+        // If the compilation failed, delete the shader.
+        if (compileStatus[0] == 0)
+        {
+            throw new RuntimeException("Error compiling shader\n" + shaderCode);
+        }
+
+        return shader;
+    }
+
+    /**
+     * Utility method for debugging OpenGL calls. Provide the name of the call
+     * just after making it:
+     *
+     * <pre>
+     * mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+     * MyGLRenderer.checkGlError("glGetUniformLocation");</pre>
+     *
+     * If the operation is not successful, the check throws an error.
+     *
+     * @param glOperation - Name of the OpenGL call to check.
+     */
+    public static void checkGlError(String glOperation) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e("GLObject", glOperation + ": glError " + error);
+            throw new RuntimeException(glOperation + ": glError " + error);
+        }
+    }
+
+    public void addObject(float[] vertexData, short[] drawOrder, float[] modelMatrix, int objectId){
+        glObjects.put(objectId,new GLObject(vertexData,drawOrder,modelMatrix, objectId));
+    }
+    public void addObject(GLObject o){
+        glObjects.put(o.objectId,o);
+    }
+    void processRawData(){
+        for(ObjectData o : rawData){
+            addObject(o.vertexData, o.drawOrder, o.modelMatrix, o.objectId);
+        }
+    }
+
+    GLObject getGLObjectById(int id){
+        return glObjects.get(id);
+    }
+}
+
+class ObjectData{
+    float[] vertexData;
+    short[] drawOrder;
+    float[] modelMatrix;
+    final int objectId;
+    ObjectData(float[] vertexData, short[] drawOrder, float[] modelMatrix, int objectId){
+        this.vertexData = vertexData;
+        this.drawOrder = drawOrder;
+        this.modelMatrix = modelMatrix;
+        this.objectId = objectId;
+    }
+}
+
+class GLObject {
+
+    FloatBuffer vertexBuffer;
+    ShortBuffer drawListBuffer;
+    final int glProgram;
+    final int objectId;
+    static final int DIMENSIONS = 3;
+    static final int BYTES_PER_FLOAT = 4;
+    static final int BYTES_PER_SHORT = 2;
+    int positionOffset = 0, positionValues = 3;
+    int colorOffset = positionValues, colorValues = 4;
+    int strideBytes = (positionValues+colorValues)*BYTES_PER_FLOAT;
+    float[] modelMatrix = new float[16];
+    float[] mvpMatrix = new float[16];
+
+    String vertexShaderCode =
+            "uniform mat4 uMVPMatrix;" +
+                    "attribute vec4 aPosition;" +
+                    "attribute vec4 aColor;" +
+                    "varying vec4 vColor;" +
+                    "void main() {" +
+                    "   vColor = aColor;" +
+                    "   gl_Position = uMVPMatrix*aPosition;" +
+                    "}";
+
+    String fragmentShaderCode =
+            "precision mediump float;" +
+                    "varying vec4 vColor;" +
+                    "void main() {" +
+                    "   gl_FragColor = vColor;" +
+                    "}";
+
+    /**
+     * @param vertexData Each vertex is defined by 7-adjacent values: X, Y, X, R, G, B, A
+     * @param drawOrder Contains indices of vertexes in draw-order. 3 per triangle, in CCW order.
+     * @param modelMatrix Position/orientation matrix for this object. Start with an identity matrix and translate/rotate it.
+     */
+    GLObject(float[] vertexData, short[] drawOrder, float[] modelMatrix, int objectId){
+
+        this.objectId = objectId;
+
+        // initialize vertex byte buffer
+        vertexBuffer = ByteBuffer
+                .allocateDirect(vertexData.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vertexBuffer.put(vertexData);
+        vertexBuffer.position(0);
+
+        // initialize byte buffer for the draw list
+        if(drawOrder != null){
+            drawListBuffer = ByteBuffer
+                    .allocateDirect(drawOrder.length * BYTES_PER_SHORT)
+                    .order(ByteOrder.nativeOrder())
+                    .asShortBuffer();
+            drawListBuffer.put(drawOrder);
+            drawListBuffer.position(0);
+        }
+
+        this.modelMatrix = modelMatrix;
+
+        //compile & load shaders
+        int vertexShader = GLRenderer.loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+        int fragmentShader = GLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+        glProgram = GLES20.glCreateProgram();
+        GLES20.glAttachShader(glProgram, vertexShader);
+        GLES20.glAttachShader(glProgram, fragmentShader);
+        GLES20.glLinkProgram(glProgram);
+    }
+
+    public int getObjectId(){
+        return objectId;
+    }
+
+    void draw(float[] vpMatrix){
+        GLES20.glUseProgram(glProgram);
+
+        //position info
+        int positionHandle = GLES20.glGetAttribLocation(glProgram, "aPosition");
+        GLES20.glEnableVertexAttribArray(positionHandle);
+        GLES20.glVertexAttribPointer(positionHandle, positionValues, GLES20.GL_FLOAT, false, strideBytes, vertexBuffer.position(0));
+
+        //color info
+        int colorHandle = GLES20.glGetAttribLocation(glProgram, "aColor");
+        GLES20.glEnableVertexAttribArray(colorHandle);
+        GLES20.glVertexAttribPointer(colorHandle, colorValues, GLES20.GL_FLOAT, false, strideBytes, vertexBuffer.position(positionValues));
+
+        //matrix transformations
+        int matrixHandle = GLES20.glGetUniformLocation(glProgram, "uMVPMatrix");
+        GLRenderer.checkGlError("glGetUniformLocation");
+        Matrix.multiplyMM(mvpMatrix, 0, modelMatrix, 0, vpMatrix, 0);
+        GLES20.glUniformMatrix4fv(matrixHandle, 1, false, mvpMatrix, 0);
+        GLRenderer.checkGlError("glUniformMatrix4fv");
+
+        //finally draw the damn thing
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, drawListBuffer.capacity());
+        GLES20.glDisableVertexAttribArray(positionHandle);
+    }
 }
--- t3d/src/main/java/com/eddiew/t3d/ogl/MainGLView.java
+++ t3d/src/main/java/com/eddiew/t3d/ogl/MainGLView.java
@@ -3,40 +3,58 @@ package com.eddiew.t3d.ogl;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+
+import java.util.ArrayList;
 
 /**
  * Contains the touch instructions for the main GLSurfaceView
  * Created by Eddie on 10/11/13.
  */
 public class MainGLView extends GLSurfaceView {
+
     GLRenderer renderer;
+
     public MainGLView(Context context, AttributeSet attrs) {
         super(context, attrs);
         // Create an OpenGL ES 2.0 context
         setEGLContextClientVersion(2);
         renderer = new GLRenderer();
-        // Test stuff. TODO: replace this
-        float[] modelMatrix = new float[16];
-        Matrix.setIdentityM(modelMatrix,0);
-        renderer.glObjects.add(new GLObject(
-           new float[]{
-               -0.5f,-0.25f,0,
-               1,0,0,1,
-
-               0.5f,-0.25f,0,
-               0,1,0,1,
-
-               0,0.559016994f,0,
-               0,0,1,1
-           },
-           new short[]{
-               0,1,2
-           },
-           modelMatrix
-        ));
         setRenderer(renderer);
+        // Test stuff. TODO: replace this
+        final float[] modelMatrix = new float[16];
+        Matrix.setIdentityM(modelMatrix, 0);
+        queueEvent(new Runnable() {
+            @Override
+            public void run() {
+                renderer.rawData.add(new ObjectData(
+                        new float[]{
+                                0f, 0.622008459f, 0,
+                                1, 0, 0, 1,
+
+                                -0.5f, -0.311004243f, 0,
+                                0, 1, 0, 1,
+
+                                0.5f, -0.311004243f, 0,
+                                0, 0, 1, 1
+                        },
+                        new short[]{
+                                0, 1, 2
+                        },
+                        modelMatrix,
+                        0));
+//                while(renderer != null){
+//                    long time = (SystemClock.uptimeMillis() % 4000);
+//                    float angle = 0.009f * time;
+//                    double disp = Math.sin(angle);
+////                    Matrix.setIdentityM(renderer.getGLObjectById(0).modelMatrix,0);
+////                    Matrix.rotateM(renderer.getGLObjectById(0).modelMatrix,0, angle, 0, 1, 0);
+////                    Matrix.translateM(renderer.getGLObjectById(0).modelMatrix, 0, 0, (float)disp,0);
+//                }
+            }
+        });
     }
     @Override
     public boolean onTouchEvent(MotionEvent event){
